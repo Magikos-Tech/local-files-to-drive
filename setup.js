17a9ab8c2cd16f41306aa2c390415e2165b699f0
unknown <karan.dayaba@magikostech.com>
2022-12-24 10:00:57 +0530

    rm secrets

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
 //parent folder on the drive
 const parent=['10D5EGpAK2ES8qb0LZ8s_kVLnHKYcNWst'];
-const logsFolder = "/upload_files";
+const logsFolder = "../paper-trade-sell-bot/options-trade-sell-bot/logs";
 
 const config = {
     parent: parent,
